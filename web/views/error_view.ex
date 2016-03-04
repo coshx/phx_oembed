0ffe5eb32c59a5ffde891d9be0fbeec0c7088779
Brad Johnson <cdale77@gmail.com>
2016-03-04 14:08:21 -0800

    removes unneeded error view code

--- web/views/error_view.ex
+++ web/views/error_view.ex
@@ -3,14 +3,4 @@ defmodule PhxOembed.ErrorView do
 
   def render("404.json", _assigns) do
   end
-
-  #TODO: Figure out why the file won't compile with this set to json
-  def render("500.html", _assigns) do
-  end
-
-  # In case no render clause matches or no
-  # template is found, let's render it as 500
-  def template_not_found(_template, assigns) do
-    render "500.html", assigns
-  end
 end
