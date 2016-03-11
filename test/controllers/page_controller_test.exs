efbe68a1d2be7118ae02bd44035057c7f02f1429
Brad Johnson <cdale77@gmail.com>
2016-03-11 14:36:42 -0800

    really add page controller test

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -1,11 +1,9 @@
 defmodule PhxOembed.PageControllerTest do
   use PhxOembed.ConnCase
-
-  setup do
-    {:ok, conn: Phoenix.ConnTest.conn()}
-  end
+  alias PhxOembed.Endpoint
 
   test "renders the front end container" do
+    conn = get conn, page_path(Endpoint, :index)
     assert html_response(conn, 200) =~ "PhxOembed"
   end
 end
