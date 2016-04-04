99a7f65d471891a20f208d448b31f50b0847b732
Brad Johnson <cdale77@gmail.com>
2016-04-03 20:57:58 -0700

    nav component

--- web/static/js/components/nav.js
+++ web/static/js/components/nav.js
@@ -1,11 +1,25 @@
 import React from "react";
 
 export default class Nav extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: false,
+      userEmail: ""
+    }
+  }
+
+  userMenu() {
+    if (this.props.loggedIn == true)
+      return(<div>Log out</div>);
+    else
+      return(<div>Log In</div>);
+  }
+
   render() {
     return(
-      <nav>
-        <div className="session-info">User Menu</div>
-      </nav>
+      <nav>{this.userMenu()}</nav>
     );
   }
 }
--- web/static/js/containers/app_container.js
+++ web/static/js/containers/app_container.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React          from "react";
 import { connect }    from "react-redux";
-import Nav from "../components/nav";
+import Nav            from "../components/nav";
 
 const mapStateToProps = () => {
   return {}
