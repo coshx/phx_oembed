c3dde02c88a9907a82f50268e9fc35799deea2cd
Brad Johnson <cdale77@gmail.com>
2016-04-06 18:49:15 -0700

    closable flash

--- web/static/js/components/flash.js
+++ web/static/js/components/flash.js
@@ -2,19 +2,44 @@ import React from "react";
 
 export default class Flash extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "",
+      flashType: ""
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      message: nextProps.message,
+      flashType: nextProps.flashType
+    });
+  }
+
   isFlashPresent() {
-    return !(this.props.message == "" || this.props.message == undefined || this.props.message == null)
+    return (this.state.message != "")
+  }
+
+  closeFlash(e) {
+    e.preventDefault();
+    this.setState({message: ""});
   }
 
   render() {
-    if(this.isFlashPresent())
+    if(this.isFlashPresent()) {
       return(
-        <div id="flash" className={"flash-" + this.props.flashType}>
-          <span className="flash-message">{this.props.message}</span>
+        <div id="flash" className={"flash-" + this.state.flashType}>
+          <span className="flash-message">{this.state.message}</span>
+          <span className="close-button">
+            <a onClick={this.closeFlash.bind(this)} href="#">X</a>
+          </span>
         </div>
       );
-    else
+    }
+    else {
       return(null);
+    }
   }
 };
 
