f65158dd38a19b7334f41e0c399480a7b49bc523
Brad Johnson <cdale77@gmail.com>
2016-04-10 19:56:57 -0700

    combine reducers

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,7 +1,7 @@
 import "phoenix_html";
 import React                            from "react";
 import ReactDOM                         from "react-dom";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import { Provider }                     from "react-redux";
 import thunk                            from "redux-thunk";
 import createLogger                     from "redux-logger";
@@ -9,6 +9,7 @@ import { Router, Route, hashHistory }   from "react-router";
 import Utils                            from "./utils";
 import Constants                        from "./constants";
 import sessionReducer                   from "./reducers/session_reducer";
+import sitesReducer                     from "./reducers/site_reducer";
 import AppContainer                     from "./containers/app_container";
 import AuthenticatedContainer           from "./containers/authenticated_container";
 import SignInContainer                  from "./containers/sign_in_container";
@@ -22,8 +23,12 @@ const defaultState = Constants.DEFAULT_STATE;
 const persistedState = JSON.parse(localStorage.getItem("appState"));
 const initialState = persistedState == null ? defaultState : persistedState;
 
+const appReducer = combineReducers(
+  {session: sessionReducer, sites: sitesReducer }
+);
+
 const logger = createLogger();
-const store = createStore(sessionReducer, initialState, applyMiddleware(thunk, logger));
+const store = createStore(appReducer, initialState, applyMiddleware(thunk, logger));
 store.subscribe(persistStore);
 
 ReactDOM.render(
