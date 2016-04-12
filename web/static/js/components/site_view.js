ee34710e793a0b65dc036ac042e8b517d62379c2
Brad Johnson <cdale77@gmail.com>
2016-04-12 15:49:24 -0700

    better site title

--- web/static/js/components/site_view.js
+++ web/static/js/components/site_view.js
@@ -4,8 +4,7 @@ export default class SiteView extends React.Component {
   render() {
     return(
       <div>
-        <h2>SiteView</h2>
-        {this.props.site.domain}
+        <h2>{this.props.site.protocol + "://" + this.props.site.domain}</h2>
       </div>
     );
   }
