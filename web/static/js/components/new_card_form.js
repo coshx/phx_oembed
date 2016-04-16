15a06b904eb868a0303307de021c685a25d13c09
Brad Johnson <cdale77@gmail.com>
2016-04-15 21:19:44 -0700

    basic new card form

--- web/static/js/components/new_card_form.js
+++ web/static/js/components/new_card_form.js
@@ -35,6 +35,37 @@ export default class NewCardForm extends React.Component {
         </a>
         <div className="form fieldset new-card-form expander-content">
           <form onSubmit={this.onSubmit.bind(this) }>
+            <label htmlFor="path">Path</label>
+            <input name="path" id="path" type="text" />
+            <label htmlFor="card_type">Type</label>
+            <input name="card_type" id="card_type" type="text" />
+            <label htmlFor="title">Title</label>
+            <input name="title" id="title" type="text" />
+            <label htmlFor="author_name">Author Name</label>
+            <input name="author_name" id="author_name" type="text" />
+            <label htmlFor="author_url">Author Url</label>
+            <input name="author_url" id="author_url" type="text" />
+            <label htmlFor="provider_name">Provider Name</label>
+            <input name="provider_name" id="provider_name" type="text" />
+            <label htmlFor="provider_url">Provider Url</label>
+            <input name="provider_url" id="provider_url" type="text" />
+            <label htmlFor="cache_age">Cache Age</label>
+            <input name="cache_age" id="cache_age" type="text" />
+            <label htmlFor="thumbnail_url">Thumb Url</label>
+            <input name="thumbnail_url" id="thumbnail_url" type="text" />
+            <label htmlFor="thumbnail_width">Thumb Width</label>
+            <input name="thumbnail_width" id="thumbnail_width" type="text" />
+            <label htmlFor="thumbnail_height">Thumb Height</label>
+            <input name="thumbnail_height" id="thumbnail_height" type="text" />
+            <label htmlFor="version">Version</label>
+            <input name="version" id="version" type="text" />
+            <label htmlFor="height">Height</label>
+            <input name="height" id="height" type="text" />
+            <label htmlFor="width">Width</label>
+            <input name="width" id="width" type="text" />
+            <label htmlFor="html">HTML</label>
+            <input name="html" id="html" type="text" />
+
             <input name="submit" id="submit" type="submit" value="Add" />
           </form>
         </div>
