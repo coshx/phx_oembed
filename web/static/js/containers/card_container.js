a28df897dcbd584a0affcb101f99c6458c5089a4
Brad Johnson <cdale77@gmail.com>
2016-04-23 17:25:22 -0700

    add basic card container

---
+++ web/static/js/containers/card_container.js
@@ -0,0 +1,20 @@
+import React            from "react";
+import { connect }      from "react-redux";
+
+const mapStateToProps = (state) => {
+
+};
+
+const mapDispatchToProps = (dispatch) => {
+
+};
+
+class CardContainer extends React.Component {
+  render() {
+    return(
+      <div>CardContainer</div>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CardContainer)
